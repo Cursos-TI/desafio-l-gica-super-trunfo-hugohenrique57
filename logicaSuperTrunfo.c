719abdc6478ec3a8eefb8bb586196a36753469bc
hugo  henrique da silva ferreira <204108947+hugohenrique57@users.noreply.github.com>
2025-04-10 01:21:55 +0000

    new commit

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -26,11 +26,95 @@ int main() {
 
     unsigned long int populacao2;
 
-     float superpoder2, densidade_inversa2;
+    float superpoder2, densidade_inversa2;
+//menu
+    int escolha;
+
+    //menu interativo 
+    printf("------- Super Tunfo -------\n");
+    printf("           MENU\n");
+    printf("(1) Iniciar\n");
+    printf("(2) Regras\n");
+    printf("(3) Sair\n");
+    scanf("%d", &escolha);
+
+    //Entrada de dados
+    //carta1-----------------------------------------
+    printf("---->Carta 1<---- \n");
+
+    printf("Digite o Estado \n:"); // uma letra de 'A' a 'H'
+    scanf(" %c", &estado1);
+
+    printf("Digite o Codigo Ex(S01) \n:");
+    scanf("%s", Cod_C1);
+
+    printf("Digite o Nome da cidade \n:");
+    scanf("%s", nome_C1 );
+
+    printf("Digite o numero de habitantes \n:");
+    scanf("%lu", &populacao1);
+
+    printf("Digite a Área (KM²) \n:");
+    scanf("%f", &area_C1);
+
+    printf("Digite o PIB \n:");
+    scanf("%f", &PIB_C1 );
+
+    printf("Digite o Numero de pontos turisticos \n:");
+    scanf("%d", &pontos_turis_C1);
+
+
+    //carta2----------------------------------------
+    printf("---->Carta 2<----\n");
+
+    printf("Digite o Estado \n:"); // uma letra de 'A' a 'H'
+    scanf(" %c", &estado2);
+
+    printf("Digite o Codigo \n:");
+    scanf("%s", Cod_C2);
+
+    printf("Digite o Nome da cidade \n:");
+    scanf("%s", nome_C2 );
+
+    printf("Digite o numero de habitantes \n:");
+    scanf("%lu", &populacao2);
+
+    printf("Digite a Área(KM²)\n:");
+    scanf("%f", &area_C2);
+
+    printf("Digite o PIB \n:");
+    scanf("%f", &PIB_C2 );
+
+    printf("Digite o Numero de pontos turisticos \n:");
+    scanf("%d", &pontos_turis_C2);
+
+    switch (escolha)
+    {
+    case 1:
+         
+    break;
+    case 2:
+        printf("A regra do jogo consiste em dois jogaores, cada um registra sua carta, e carta com maior numero pontos vence!\n");
+        printf(" o numero de pontos das cartas sao calculado pelos valores das estatisticas, sendo elas individuais ou somadas\n");
+    break;
+    case 3:
+        printf("SAINDO...\n");
+    break;
+    default:
+        break;
+    }
+
+    printf("Quais valores deseja comparar\n");
+    printf("(1) Para comparar População\n");
+    printf("(2) para comparar Area\n");
+    printf("(3) para comparar PIB\n");
+    printf("(4) para comparar numeros de Pontos turisticos\n");
+    printf("(5) para comparar densidade Populacional\n");
+    printf("(6) para comparar PIB per Capta")
 
 //Entrada de dados
 //carta1-----------------------------------------
-    printf("---->Carta 1<---- \n");
+/*  printf("---->Carta 1<---- \n");
 
     printf("Digite o Estado \n:"); // uma letra de 'A' a 'H'
     scanf(" %c", &estado1);
@@ -77,7 +161,7 @@ int main() {
 
     printf("Digite o Numero de pontos turisticos \n:");
     scanf("%d", &pontos_turis_C2);
-
+*/
 //comparacao----------------------------------------------------------------------
  
     printf("--------Comparando Cartas--------\n");
